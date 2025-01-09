a787b5baba2d6377c7ee61ea2dcee1bd1f9eecbe
Borlakunta Soniya <155159154+Soniya-321@users.noreply.github.com>
2025-01-09 16:20:17 +0530

    RegistrationForm.js

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import './styles.css'
 
 const RegistrationForm = () => {
@@ -20,6 +21,7 @@ const RegistrationForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setMessage('');
+    console.log('clicked');
 
     try {
       const response = await fetch('https://backend-app-c5d0.onrender.com/api/users', {
@@ -31,7 +33,7 @@ const RegistrationForm = () => {
       });
 
       const data = await response.json();
-      console.log(data);
+      console.log(data, "data");
       if (response.ok) {
         setMessage('Registration successful!');
         setTimeout(() => navigate('/login'), 1500); // Redirect to login
@@ -43,6 +45,11 @@ const RegistrationForm = () => {
     }
   };
 
+  const handleSocialLogin = (provider) => {
+    // Placeholder logic for social login
+    console.log(`Login with ${provider}`);
+  };
+
   return (
     <div className="form-container">
       <h2>Register</h2>
@@ -52,9 +59,23 @@ const RegistrationForm = () => {
         <input type="text" name="lastName" placeholder="Last Name" required onChange={handleChange} />
         <input type="text" name="mobileNumber" placeholder="Mobile Number" required onChange={handleChange} />
         <input type="password" name="password" placeholder="Password" required onChange={handleChange} />
-        <button type="submit">Register</button>
+        <button className='register-btn' type="submit">Sign In</button>
       </form>
       {message && <p>{message}</p>}
+      <div className="social-login">
+        <p>Or login with:</p>
+        <div className='icons-container'>
+          <button className="social-button" onClick={() => handleSocialLogin('Google')}>
+            <img src='https://www.svgrepo.com/show/303108/google-icon-logo.svg' alt='google' className='google'/>
+          </button>
+          <button className="social-button" onClick={() => handleSocialLogin('Facebook')}>
+            <img src='https://www.svgrepo.com/show/303114/facebook-3-logo.svg' alt='facebook' className='facebook'/>
+          </button>
+          <button className="social-button" onClick={() => handleSocialLogin('Apple')}>
+            <img src='https://www.svgrepo.com/show/303110/apple-black-logo.svg' alt='apple' className='apple'/>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
