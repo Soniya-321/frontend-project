2e9817e5206fc86d3c9efeea9e070de8bd8ea9da
Borlakunta Soniya <155159154+Soniya-321@users.noreply.github.com>
2025-01-09 10:23:26 +0530

    LoginForm.js

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,7 +16,7 @@ const LoginForm = () => {
     setMessage('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://backend-app-c5d0.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
